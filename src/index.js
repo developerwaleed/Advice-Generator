24360ed7c86cda60b5021128d41bb7cbb41cec34
Waleed Amjad <waleedamjad99@hotmail.com>
2022-06-20 20:48:57 +0500

    Finalized the solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import './style.css';
 import patternMobile from './assets/patternMobile.svg';
+import patternDesktop from './assets/patternDesktop.svg';
 import icondice from './assets/icondice.svg';
 
-const divider = document.getElementById('divide');
+const dividerMobile = document.getElementById('divide-Mobile');
+const dividerDesktop = document.getElementById('divide-Desktop');
 const dice = document.getElementById('dice');
-divider.src = patternMobile;
+const advice = document.getElementById('advice');
+const adviceId = document.getElementById('advice-id');
+const generatorBtn = document.getElementById('generator-btn');
+
+dividerMobile.src = patternMobile;
+dividerDesktop.src = patternDesktop;
+
 dice.src = icondice;
 
+const getData = async () => {
+  const response = await fetch(`https://api.adviceslip.com/advice`);
+  if (!response.ok) {
+    throw new Error(`Something Went Wrong status: ${response.status}`);
+  }
+  const data = await response.json();
+  advice.innerHTML = data.slip.advice;
+  adviceId.innerHTML = data.slip.id;
+};
+
+generatorBtn.addEventListener('click', (e) => getData());
+getData();
